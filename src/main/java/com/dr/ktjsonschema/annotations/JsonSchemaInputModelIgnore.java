d8d917399925079b8ac0fb8eaddfeb7bdbf8ff22
Evgeny Zavalkovsky <zavale@gmail.com>
2018-07-13 14:12:58 -0700

    Annotations added

---
+++ src/main/java/com/dr/ktjsonschema/annotations/JsonSchemaInputModelIgnore.java
@@ -0,0 +1,11 @@
+package com.dr.ktjsonschema.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({ METHOD, FIELD, PARAMETER, TYPE })
+@Retention(RUNTIME)
+public @interface JsonSchemaInputModelIgnore{}
